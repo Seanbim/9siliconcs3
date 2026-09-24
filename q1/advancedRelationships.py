ed731dd253128522e5ca17f39cb38c8ee08ad3a4
Seanbim <njlbriones@brc.pshs.edu.ph>
2026-09-24 20:26:31 +0800

    Update advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         skill_type="Ice Shard",
         mana_cost=15,
         dmg=20,
-        stun=True
+        stun=True,
         targeting_type="Auto Targeting"
     )
 
